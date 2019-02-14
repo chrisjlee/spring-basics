f50f1510370bf98400ec71436d7145e3634ad95e
Chris Lee (PLJ6915) <christopher.lee@usaa.com>
2019-02-14 13:52:34 -0600

    Initial commit

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/math/pi")
+    public String helloWorld() {
+        return "3.141592653589793";
+    }
+
+}
