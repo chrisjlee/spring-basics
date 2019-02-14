bb79f4bec2fefe3a44ff9de82491c6ec07a792b3
Chris Lee (PLJ6915) <christopher.lee@usaa.com>
2019-02-14 13:54:17 -0600

    Updated the correct paths

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/math/pi")
+    @GetMapping("/")
     public String helloWorld() {
-        return "3.141592653589793";
+        return "Hello from Spring!";
     }
 
 }
